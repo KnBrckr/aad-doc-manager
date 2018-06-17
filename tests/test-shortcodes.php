5a64ad3fabf7f78be2e387f04fed5aed036cdba7
Kenneth J. Brucker <ken@pumastudios.com>
2018-06-17 08:00:00 -0700

    Add templates for additional unit tests of shortcodes.
    More implementation work needed!

--- tests/test-shortcodes.php
+++ tests/test-shortcodes.php
@@ -25,9 +25,14 @@ class TestShortcodes extends WP_UnitTestCase {
 	protected static $normal_post_id;
 
 	/**
-	 * @var int  PostID for a normal document
+	 * @var int  PostID for a published CSV document
 	 */
-	protected static $document_post_id;
+	protected static $document_csv_post_id;
+
+	/**
+	 * @var int PostID for a published PDF document
+	 */
+	protected static $document_pdf_post_id;
 
 	/**
 	 * @var int PostID for a document revision
@@ -45,13 +50,19 @@ class TestShortcodes extends WP_UnitTestCase {
 		/**
 		 * A valid document
 		 */
-		$doc_attrs				 = [
+		$doc_attrs					 = [
 			'post_type'		 => Document::POST_TYPE,
 			'post_date'		 => self::DATESTAMP,
 			'post_date_gmt'	 => self::DATESTAMP_GMT,
 			'post_mime_type' => 'text/csv'
 		];
-		self::$document_post_id	 = $factory->post->create( $doc_attrs );
+		self::$document_csv_post_id	 = $factory->post->create( $doc_attrs );
+
+		/**
+		 * PDF mime-type
+		 */
+		$doc_attrs['post_mime_type'] = 'application/pdf';
+		self::$document_pdf_post_id	 = $factory->post->create( $doc_attrs );
 
 		/**
 		 * An older revision
@@ -61,9 +72,9 @@ class TestShortcodes extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test SCCSVTable shortcode
+	 * Test SCCSVTable shortcode with invalid document
 	 */
-	public function test_SCCSVTable() {
+	public function test_sc_csv_table_empty() {
 		/**
 		 * Null input returns empty string
 		 */
@@ -97,20 +108,99 @@ class TestShortcodes extends WP_UnitTestCase {
 		/**
 		 * Wrong mime type
 		 */
-		/**
-		 * Test Valid Table
-		 */
-		$attrs['id'] = self::$document_post_id;
-//		$attrs	 = [ 'id' => 1 ];
-//		$result	 = SCCSVTable::sc_docmgr_csv_table( $attrs );
-//		$result	 = 'FIXME';
-//		$this->assertStringMatchesFormat( '<div class="aad-doc-manager"><table class="aad-doc-manager-csv searchHighlight responsive no-wrap" width="100%"><caption>%a</caption>%a</table></div>', $result, "Valid document post type" );
+		$attrs['id'] = self::$document_pdf_post_id;
+		$this->assertEquals( "", SCCSVTable::sc_docmgr_csv_table( $attrs ), "Wrong mime type" );
 	}
 
 	/**
-	 * Test SCCreated shortcode
+	 * Test SCCSVTable shortcode with invalid document
 	 */
-	function test_SCCreated() {
+	function test_sc_csv_table_simple() {
+		self::markTestIncomplete();
+
+		/**
+		 * Test Valid Table
+		 */
+		$attrs = [ 'id' => self::$document_csv_post_id ];
+		$result	 = SCCSVTable::sc_docmgr_csv_table( $attrs );
+		$expected = '<div class="aad-doc-manager"><table class="aad-doc-manager-csv searchHighlight responsive no-wrap" width="100%"><caption>%a</caption>%a</table></div>';
+		$this->assertStringMatchesFormat( $expected, $result, "Valid document post type" );
+	}
+
+	/**
+	 * Test enqueuing of datables JS script
+	 */
+	function test_action_enqueue_datatables() {
+		self::markTestIncomplete();
+	}
+
+	/**
+	 * Test init of datatables
+	 */
+	function test_action_init_datatables() {
+		self::markTestIncomplete();
+	}
+
+	/**
+	 * Test CSV storage format 1
+	 */
+	function test_sc_csv_table_format_1() {
+		self::markTestIncomplete();
+	}
+
+	/**
+	 * Test CSV storage format 2
+	 */
+	function test_sc_csv_table_format_2() {
+		self::markTestIncomplete();
+	}
+
+	/**
+	 * Test CSV storage format 3
+	 */
+	function test_sc_csv_table_format_3() {
+		self::markTestIncomplete();
+	}
+
+	/**
+	 * Test date option in csv_table
+	 */
+	function test_sc_csv_table_date() {
+		self::markTestIncomplete();
+	}
+
+	/**
+	 * Test row-colors option in csv_table
+	 */
+	function test_sc_csv_table_row_colors() {
+		self::markTestIncomplete();
+	}
+
+	/**
+	 * Test row-number option in csv_table
+	 */
+	function test_sc_csv_table_row_number() {
+		self::markTestIncomplete();
+	}
+
+	/**
+	 * Test page-length option in csv_table
+	 */
+	function test_sc_csv_table_page_length() {
+		self::markTestIncomplete();
+	}
+
+	/**
+	 * Test rows option in csv_table
+	 */
+	function test_sc_csv_table_rows() {
+		self::markTestIncomplete();
+	}
+
+	/**
+	 * Test SCCreated shortcode with invalid id
+	 */
+	function test_sc_created_empty() {
 
 		/**
 		 * Null Input
@@ -141,18 +231,23 @@ class TestShortcodes extends WP_UnitTestCase {
 		 */
 		$attrs['id'] = self::$document_revision_post_id;
 		$this->assertEquals( "", SCCreated::sc_docmgr_created( $attrs ), "Wrong post status" );
+	}
 
+	/**
+	 * Test SCCreated shortcode with invalid id
+	 */
+	function test_sc_created() {
 		/**
 		 * Valid Document
 		 */
-		$attrs['id'] = self::$document_post_id;
+		$attrs['id'] = self::$document_csv_post_id;
 		$this->assertEquals( 'June 13, 2018', SCCreated::sc_docmgr_created( $attrs ), "Valid post" );
 	}
 
 	/**
-	 * Test SCModified shortcode
+	 * Test SCModified shortcode with invalid document
 	 */
-	function test_SCModified() {
+	function test_sc_modified_empty() {
 
 		/**
 		 * Null Input
@@ -183,18 +278,23 @@ class TestShortcodes extends WP_UnitTestCase {
 		 */
 		$attrs['id'] = self::$document_revision_post_id;
 		$this->assertEquals( '', SCModified::sc_docmgr_modified( $attrs ), "Wrong post status" );
+	}
 
+	/**
+	 * Test SCModified shortcode with valid document
+	 */
+	function test_sc_modified() {
 		/**
 		 * Valid document
 		 */
-		$attrs['id'] = self::$document_post_id;
+		$attrs = [ 'id' => self::$document_csv_post_id ];
 		$this->assertEquals( "June 13, 2018", SCModified::sc_docmgr_modified( $attrs ), "Valid post" );
 	}
 
-		/**
-	 * Test SCModified shortcode
+	/**
+	 * Test SCModified shortcode with invalid ID
 	 */
-	function test_SCDownloadURL() {
+	function test_sc_download_url_empty() {
 
 		/**
 		 * Null Input
@@ -225,13 +325,20 @@ class TestShortcodes extends WP_UnitTestCase {
 		 */
 		$attrs['id'] = self::$document_revision_post_id;
 		$this->assertEquals( '', SCDownloadURL::sc_docmgr_download_url( $attrs ), "Wrong post status" );
+	}
+
+	/**
+	 * Test SCModified shortcode with valid ID
+	 */
+	function test_sc_download_url() {
+		self::markTestIncomplete();
 
 		/**
 		 * Valid document
 		 */
-//		$attrs['id'] = self::$document_post_id;
-//		$this->assertEquals( "June 13, 2018", SCDownloadURL::sc_docmgr_download_url( $attrs ), "Valid post" );
+		$attrs['id'] = self::$document_pdf_post_id;
+		$expected = 'a url';
+		$this->assertEquals( $expected, SCDownloadURL::sc_docmgr_download_url( $attrs ), "Valid download URL" );
 	}
-
 
 }
